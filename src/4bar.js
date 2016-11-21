1de2ec1a667da20b3978c803f92ae785f87a704d
Naoki Nishida <domitry@gmail.com>
2016-11-21 19:24:11 +0900

    Add a 4bar linkage simulator

---
+++ src/4bar.js
@@ -0,0 +1,15 @@
+/*global _*/
+
+function four_bar(a, b, c, theta1){
+    var cos = Math.cos, sin = Math.sin, atan = Math.atan, sqrt=Math.sqrt;
+    
+    return _.map(theta1, function(t1){
+        var k1=1./a, k2=1./c;
+        var k3=(a*a-b*b+c*c+1)/(2*a*c);
+        var D = cos(t1)-k1+k2*cos(t1)+k3;
+        var E = -2*sin(t1);
+        var F = k1-(k2+1)*cos(t1)+k3;
+        if(E*E-4*D*F<0.)console.warn("E*E<4*D*F");
+        return 2*atan((-E+sqrt(E*E-4*D*F))/(2*D));
+    });
+}
