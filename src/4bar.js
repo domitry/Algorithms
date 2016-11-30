cf86f9cc68d3ed8da12dadd69b6498cefaca0fb4
Naoki Nishida <domitry@gmail.com>
2016-11-30 19:16:38 +0900

    Fix bug in four bar linkage

--- src/4bar.js
+++ src/4bar.js
@@ -4,6 +4,7 @@ function four_bar(a, b, c, theta1, pm){
     var cos = Math.cos, sin = Math.sin, atan = Math.atan, sqrt=Math.sqrt;
     
     return _.map(theta1, function(t1){
+        /*
         var k1=1./a, k2=1./c;
         var k3=(a*a-b*b+c*c+1)/(2*a*c);
         var D = cos(t1)-k1+k2*cos(t1)+k3;
@@ -11,5 +12,13 @@ function four_bar(a, b, c, theta1, pm){
         var F = k1-(k2+1)*cos(t1)+k3;
         if(E*E-4*D*F<0.)console.warn("E*E<4*D*F");
         return 2*atan((-E+ (pm?1:-1)*sqrt(E*E-4*D*F))/(2*D));
+         */
+
+        var p = Math.PI - t1;
+        var A = sin(p);
+        var B = (1./a)+cos(p);
+        var C = (1./c)*cos(p) + (a*a-b*b+c*c+1)/(2*a*c);
+        var ps = 2*atan((A+(pm?1:-1)*sqrt(A*A+B*B-C*C))/(B+C));
+        return Math.PI-ps;
     });
 }
